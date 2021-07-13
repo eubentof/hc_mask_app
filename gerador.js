6df5b2ac0926e71217130e5934e9394ed6367942
Bento <dev.bento@gmail.com>
2021-07-13 16:48:07 -0300

    changes 2

--- gerador.js
+++ gerador.js
@@ -1,71 +1,278 @@
-const canvas = document.getElementById("mask");
+const section = document.getElementById("section");
+const selectedFilter = document.getElementById("active-filter");
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext('2d');
+
 const filters = document.getElementById("filters");
 const btnClose = document.getElementById('close');
 const popup = document.getElementById('popup');
-const canvasWidth = document.getElementById('canvas-width');
 
-// canvas.addEventListener("mousemove", e => {
-//   if (this.active) {
-//     var rect = canvas.getBoundingClientRect();
-//     var x = e.clientX - rect.left;
-//     var y = e.clientY - rect.top;
-//     x = Math.floor(this.width * x / canvas.clientWidth);
-//     y = Math.floor(this.height * y / canvas.clientHeight);
-//     if (tools[Tool.pen]) {
-//       this.draw(x, y)
-//     }
-//     else if (tools[Tool.eraser]) {
-//       this.erase(x, y);
-//     }
-//   }
-// });
+const canvasWidthInput = document.getElementById('canvas-width');
+const menuButtons = document.getElementById('menu');
 
-function showContent() {
-  var filter_template = document.getElementsByTagName("template")[0];
-  var filter = filter_template.content.querySelector('div');
-  for (let i = 5; i > 0; i--) {
-    const item = document.importNode(filter, true);
-    item.textContent = `${2 ** i}x${2 ** i}`;
-    item.style.width = 2 ** i * 10;
-    item.style.height = 2 ** i * 10;
-    console.log(item);
-    filters.appendChild(item);
-  }
+let filterSize = null;
+let canvasSize = 0;
+
+let mask = [];
+const colorsPerFilterSize = {};
+
+const menuClickActions = {
+  'eraser': allowErasing,
+  'clear': clearMask,
+  'export-txt': exportMask,
+  'export-png': exportImage,
+}
+
+function map(number, inMin, inMax, outMin, outMax) {
+  return Math.floor((number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin);
 }
 
 btnClose.addEventListener('click', () => {
-  const width = Number(canvasWidth.value);
+
   popup.classList.add('hide');
   canvas.style.display = 'block';
-  showContent()
+  canvasSize = Number(canvasWidthInput.value);
+  mask = Array.from(new Array(canvasSize), () => new Array(canvasSize).fill(0));
+  section.style.width = `${canvasSize * 2}px`
+  section.style.height = `${canvasSize * 2}px`
+
+  canvas.style.width = `${canvasSize * 2}px`
+  canvas.width = canvasSize * 2;
+  canvas.style.height = `${canvasSize * 2}px`
+  canvas.height = canvasSize * 2;
+  canvas.style.opacity = 1;
+
+  ctx.beginPath(); //Start path
+  ctx.fillStyle = 'white';
+  ctx.fillRect(0, 0, canvasSize * 2, canvasSize * 2);
+  ctx.closePath();
+  ctx.stroke();
+
+  menuButtons.style.display = 'flex';
+  const children = menuButtons.children;
+  for (var i = 0; i < children.length; i++) {
+    const child = children[i];
+    child.addEventListener('click', menuClickActions[child.id]);
+  }
+
+  getFilters()
 })
 
-class Canvas {
-  constructor(width, height) { }  // initialize all canvas properties
+function getFilters() {
+  var filter_template = document.getElementsByTagName("template")[0];
+  var filter = filter_template.content.querySelector('div');
+  let i = 2;
+  while (i < canvasSize) {
+    colorsPerFilterSize[i] = `hsl(${map(i === 2 ? 0 : i * 3, 0, canvasSize, 0, 365)}, 50%, 50%)`
+    const item = document.importNode(filter, true);
+    item.innerHTML = `
+      <div class="filter" style="width: ${i}px; height: ${i}px; background-color:${colorsPerFilterSize[i]};"></div>
+      <p style="color: white">${i}x${i}</p>
+    `;
+    item.dataset.size = i;
+    item.addEventListener('click', selectFilter)
+    filters.appendChild(item);
+    i *= 2;
+  }
+}
 
-  draw(x, y) { }  // method to draw a pixel on canvas
+function selectFilter(e) {
+  filterSize = Number(e.target.parentNode.dataset.size);
+  selectedFilter.style.width = `${filterSize * 2}px`;
+  selectedFilter.style.height = `${filterSize * 2}px`;
+  selectedFilter.style.opacity = .5;
+  selectedFilter.style.backgroundColor = colorsPerFilterSize[filterSize];
+}
 
-  erase(x, y) { }  // method to erase a pixel on canvas
+function findCell(x, y) {
+  let sliceSize = canvasSize / 2;
+  let startX = 0;
+  let startY = 0;
 
-  setcolor(color) { }  // method to set the current color
+  while (sliceSize >= filterSize) {
+    // 4th quad
+    startX += sliceSize;
+    startY += sliceSize;
 
-  setmode(i) { }  // method to set the active tool
+    // 1st quad
+    if (y <= startY) {
+      startY -= sliceSize;
 
-  save() { }  // method to save pixel art as image
+      // 2dn quad
+      if (x <= startX) {
+        startX -= sliceSize;
+      }
+      // 3rd quad
+    } else if (x <= startX) startX -= sliceSize;
 
-  clear() { }  // method to clear canvas
+    sliceSize /= 2;
+  }
 
-  addFrame() { }  // method to add current frame to frame list
+  return { startX, startY }
+}
 
-  deleteFrame(f) { }  // method to delete a specific frame
+function getCoords(e) {
+  const x = map(e.layerX, 0, canvas.clientWidth, 0, canvasSize);
+  const y = map(e.layerY, 0, canvas.clientHeight, 0, canvasSize);
+  return findCell(x, y)
+}
 
-  loadFrame(f) { }  // method to load a specific frame onto canvas
+let activeRegion = {};
+let isDown = false;
+let isValid = false;
 
-  renderGIF() { }  // method to render a GIF using frames
+// Handles the movement of the filter
+canvas.addEventListener('mousemove', mouseMove);
+function mouseMove(e) {
+  if (filterSize) {
+    activeRegion = getCoords(e);
+    selectedFilter.style.top = `${activeRegion.startY * 2}px`;
+    selectedFilter.style.left = `${activeRegion.startX * 2}px`;
+    checkFilter(e);
+  }
+}
 
-  undo() { }  // method to undo a given step
+// Handles the action of filling a part of the mask
+selectedFilter.addEventListener('mousedown', () => isDown = true);
+selectedFilter.addEventListener('mouseup', () => isDown = false);
+selectedFilter.addEventListener('click', addFilter);
+selectedFilter.addEventListener('mousemove', () => checkFilter);
 
-  redo() { }  // method to redo a given step
+function checkFilter(e) {
+  const shift = e.getModifierState("Shift");
+  const ctrl = e.getModifierState("Control");
+  const alt = e.getModifierState("Alt");
 
-  addImage() { }  // method to load an image as pixel art
+  const cellIsNotEmpty = mask[activeRegion.startX][activeRegion.startY] > 0;
+  const filterIsLower = mask[activeRegion.startX][activeRegion.startY] > filterSize;
+  selectedFilter.style.cursor = 'crosshair';
+
+  // if ((cellIsNotEmpty || filterIsLower) && !ctrl) {
+  //   selectedFilter.style.cursor = 'not-allowed';
+  //   isValid = false;
+  //   return
+  // }
+
+  isValid = true;
+  if (isDown) addFilter(e);
+}
+
+function addFilter(e) {
+  if (!isValid) return
+  const shift = e.getModifierState("Shift");
+  const ctrl = e.getModifierState("Control");
+  const alt = e.getModifierState("Alt");
+
+  if (isErasing) clearFilter()
+  else if (ctrl) {
+    if (shift) fillAvailableArea();
+    if (!shift) clearFilter();
+  } else {
+    drawFilter();
+  }
+
+}
+
+function clearFilter() {
+  if (!filterSize) return
+  drawFilter({ color: 'white', erase: true })
+}
+
+function fillAll() {
+  ctx.beginPath(); //Start path
+  ctx.fillStyle = colorsPerFilterSize[filterSize];
+  ctx.fillRect(0, 0, canvasSize * 2, canvasSize * 2);
+  ctx.closePath();
+  ctx.stroke();
+}
+
+function fillAvailableArea() {
+  for (let startX = 0; startX <= (canvasSize - filterSize); startX += filterSize) {
+    for (let startY = 0; startY <= (canvasSize - filterSize); startY += filterSize) {
+      if (mask[startX][startY] === 0) {
+        drawFilter({ startX, startY })
+      }
+    }
+  }
+}
+
+function drawFilter(params) {
+
+  let { startX, startY, size, color, erase } = params || {};
+
+  if (size === undefined) size = filterSize
+  if (startX === undefined) startX = activeRegion.startX;
+  if (startY === undefined) startY = activeRegion.startY;
+  if (color === undefined) color = colorsPerFilterSize[filterSize];
+
+  ctx.beginPath(); //Start path
+  ctx.fillStyle = color;
+  ctx.fillRect(startX * 2, startY * 2, size * 2, size * 2);
+  ctx.closePath();
+  ctx.stroke();
+
+  for (let i = startX; i < startX + size; i++) {
+    for (let j = startY; j < startY + size; j++) {
+      mask[i][j] = erase ? 0 : size;
+    }
+  }
+}
+
+
+function clearMask() {
+  ctx.beginPath(); //Start path
+  ctx.fillStyle = 'white';
+  ctx.fillRect(0, 0, canvasSize * 2, canvasSize * 2);
+  ctx.closePath();
+  ctx.stroke();
+  mask = Array.from(new Array(canvasSize), () => new Array(canvasSize).fill(0));
+}
+
+function exportMask() {
+  let file = new Blob([JSON.stringify(mask)], { type: 'txt' });
+  let blobUrl = URL.createObjectURL(file);
+  download(blobUrl, 'mask.txt');
+}
+
+function exportImage() {
+  var myImage = canvas.toDataURL("image/png");
+  download(myImage, 'mask.png')
+}
+
+function download(href, file_name) {
+  // if (typeof navigator.msSaveBlob == "function")
+  // return navigator.msSaveBlob(blob, file_name);
+
+  var saver = document.createElementNS("http://www.w3.org/1999/xhtml", "a");
+  var blobURL = saver.href = href, body = document.body;
+
+  saver.download = file_name;
+
+  body.appendChild(saver);
+  saver.dispatchEvent(new MouseEvent("click"));
+  body.removeChild(saver);
+  URL.revokeObjectURL(blobURL);
+}
+
+// [x] - Preencher a mascara conforme o filtro.
+// [x] - Olhar uma forma mais eficiente de encontrar a posição no grid.
+// [x] - Add o botão de reset
+// [x] - Add o botão de export
+// [] - Ao clicar com o shift, ir preenchendo uma área.
+// [] - Add um título para a página
+// [] - Add um footer com o "created by Filipe Bento"
+
+let isErasing = false;
+function allowErasing(e) {
+  let target = e.target;
+  if (target.id !== "eraser") target = target.parentElement
+  console.log(target);
+  if (isErasing) {
+    isErasing = false;
+    target.classList.remove('active');
+  } else {
+    isErasing = true;
+    target.classList.add('active');
+  }
+  console.log(isErasing);
 }
